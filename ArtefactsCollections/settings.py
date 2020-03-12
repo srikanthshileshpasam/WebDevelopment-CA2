0a1ae5ec9c8650118a429e82db39b405add0eaed
Srikanth Pasam <10387794@mydbs.ie>
2020-03-12 23:12:51 +0000

    White Noise Removed

--- ArtefactsCollections/settings.py
+++ ArtefactsCollections/settings.py
@@ -17,7 +17,7 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
 
@@ -131,7 +131,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
-# STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'), )
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'), )
 
 
 MESSAGE_STORAGE = 'django.contrib.messages.storage.session.SessionStorage'
