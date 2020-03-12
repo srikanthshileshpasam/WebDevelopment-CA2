1951b7650060c7127abcd11531ee1221431e155a
Srikanth Pasam <10387794@mydbs.ie>
2020-03-12 10:53:27 +0000

    kudu build push

--- ArtefactsCollections/settings.py
+++ ArtefactsCollections/settings.py
@@ -67,7 +67,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
-                'django.template.context_processors.media',                
+                'django.template.context_processors.media',
             ],
         },
     },
@@ -85,7 +85,6 @@ DATABASES = {
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
