94f8e5126c20cd90afe4985d9fd440a9a70b041f
Srikanth Pasam <10387794@mydbs.ie>
2020-03-12 12:09:15 +0000

    Update settings.py

--- ArtefactsCollections/settings.py
+++ ArtefactsCollections/settings.py
@@ -68,7 +68,6 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
                 'django.template.context_processors.media',
-                'timeout': 200,
             ],
         },
     },
